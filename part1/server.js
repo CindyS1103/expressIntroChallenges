dd26ded8552dab2c12c53c55ca5398955626218a
Cindy Schlegel <invertsor@gmail.com>
2018-03-29 18:18:32 -0700

    final

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const bodyParser = require('body-parser');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.use(bodyParser.json());
+
+app.get('/hello', function(req, res) {
+  let string = `"Hello!" ${req.params.hello}`
+  res.send(string);
+});
+
+app.post('/create/:name', function(req, res) {
+  let obj = {
+    id:1,
+    name: req.params.name
+  }
+  res.json(obj);
+});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/verify/:age', function(req, res) {
+  if(req.params.age > 13){
+    res.status(200).send('all good');
+  } else if(req.params.age < 13){
+    res.status(403).send('no good');
+  }
 });
 
 app.use(function(req, res) {
--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.use(bodyParser.json());
+
+app.post('/create/:name/:age', function(req, res) {
+  let object = {
+    name: req.params.name,
+    age: req.params.age
+  }
+
+  fs.writeFileSync('./storage.json', JSON.stringify(object));
+
+  res.json(object);
+
+});
+app.get('/', function(req, res) {
+  let storageData = fs.readFileSync('./storage.json', 'utf8');
+  res.send(storageData);
+});
+app.get('/:name', function(req, res) {
+
+  let storageData2 = JSON.parse(fs.readFileSync('./storage.json', 'utf8'));
+  let filteredData = storageData2.filter(person => person.name === req.params.name)
+  if(filteredData.length){
+    res.send(filteredData[0]);
+  }else {
+    res.status(400).send('no good');
+  }
+
 });
 
 app.use(function(req, res) {
